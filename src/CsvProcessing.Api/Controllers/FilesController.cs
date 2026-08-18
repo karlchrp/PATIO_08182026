22908adb2a80f573349cb79f3649c3d6f98408e7
Karl Patio <kpatio@iqbackoffice.com>
2026-08-18 13:13:32 +0800

    Added class library for services.

---
+++ src/CsvProcessing.Api/Controllers/FilesController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace CsvProcessing.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/files")]
+public class FilesConrtoller : ControllerBase
+{
+    [HttpPost("process")]
+    public async Task<ActionResult<int>> ProcessAsync()
+    {
+        return Ok(1);
+    }
+}
---
+++ src/CsvProcessing.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CsvProcessing.Application
+{
+    public class Class1
+    {
+
+    }
+}
